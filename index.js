d85df7fbd1083e20e7f6804810bb2c7b3ed725dc
Sophia Brandt <github@s-br.net>
2019-02-19 12:37:55 +0100

    [add] passport-next, google auth, nodemon

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+require('dotenv').config()
 const express = require('express')
+const passport = require('@passport-next/passport')
+const GoogleStrategy = require('@passport-next/passport-google-oauth2').Strategy
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:5000/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken, refreshToken, profile, done)
+    }
+  )
+)
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/')
+  }
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
