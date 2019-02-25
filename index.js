ba850260b87ccd1942e41b7acc90398fdf7685e3
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-02-25 08:49:24 +0100

    [add] client: Landing component, React Router logic to Header.js, [update] mongoose connect on backend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 // Mongoose
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 require('./models/User')
 
 // Cookie Session
