9e66403441e2ef10734f3e9f83eb193cad987273
Sophia Brandt <tech@s-br.net>
2019-02-25 08:49:24 +0100

    [add] client: Landing component, React Router logic to Header.js, [update] mongoose connect on backend

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   renderContent() {
-    console.log(this.props.auth)
     switch (this.props.auth) {
       case null:
         return
@@ -25,9 +25,11 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper green lighten-3">
-          <a href="#" className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo">
             Feedbacky
-          </a>
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
@@ -35,8 +37,6 @@ class Header extends Component {
   }
 }
 
-function mapStateToProps({ auth }) {
-  return { auth }
-}
+const mapStateToProps = ({ auth }) => ({ auth })
 
 export default connect(mapStateToProps)(Header)
