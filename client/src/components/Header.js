c65de08c709ae81419409c3ca8dc0cac1ebf1d57
Sophia Brandt <tech@s-br.net>
2019-02-28 17:53:13 +0100

    [add] begin survey route, mongoose Schema for surveys

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component {
             <Payments />
           </li>,
           <li key="credits" style={{ margin: '0 10px' }}>
-            Credits: {this.props.auth.credits}{' '}
+            Credits: {this.props.auth.credits}
           </li>,
           <li key="logout">
             <a href="/api/logout">Logout</a>
