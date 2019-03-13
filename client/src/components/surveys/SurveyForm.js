40d2c2887a35e6020fa7dcd150f2803670176f4d
Sophia Brandt <tech@s-br.net>
2019-03-13 17:36:13 +0100

    👌 IMPROVE: improve SurveyForm, add tern.js

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,48 +1,36 @@
-import React, { Component } from 'react'
-import { reduxForm, Field } from 'redux-form'
+import React from 'react'
+import { reduxForm, Field, FieldArray } from 'redux-form'
 import SurveyField from './SurveyField'
 
-class SurveyForm extends Component {
-  renderFields() {
-    return (
-      <div>
-        <Field
-          label="Survey Title"
-          type="text"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          label="Subject Line"
-          type="text"
-          name="subject"
-          component={SurveyField}
-        />
-        <Field
-          label="Email Body"
-          type="text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="text"
-          name="recipients"
-          component={SurveyField}
-        />
-      </div>
-    )
-  }
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          {this.renderFields()}
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    )
-  }
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'recipients' }
+]
+
+const renderFields = () => (
+  <div>
+    {FIELDS.map(({ name, label }) => (
+      <Field
+        key={name}
+        name={name}
+        type="text"
+        component={SurveyField}
+        label={label}
+      />
+    ))}
+  </div>
+)
+
+const SurveyForm = props => {
+  const { handleSubmit } = props
+  return (
+    <form onSubmit={handleSubmit(values => console.log(values))}>
+      <FieldArray name="survey" component={renderFields} />
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
 
 export default reduxForm({
