b5e6beec3d52cd72134ea7da96db990dcef7cfc0
Sophia Brandt <tech@s-br.net>
2019-03-17 17:34:40 +0100

    📦 NEW: add new redux action and wire it up to SurveyFormReview

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import formFields from './formFields'
+import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = formFields.map(({ name, label }) => {
   return (
   <div key={name}>
@@ -21,7 +22,7 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
         Back
       </button>
-      <button className="green btn-flat right white-text">
+      <button onClick={() => submitSurvey(formValues)} className="green btn-flat right white-text">
         Send Survey
         <i className="material-icons right white-text">email</i>
       </button>
@@ -33,4 +34,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values }
 }
 
-export default connect(mapStateToProps)(SurveyFormReview)
+export default connect(mapStateToProps, actions)(SurveyFormReview)
