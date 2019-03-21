fea2059e3b74265f20fb54ef06eff4bdc00ec679
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-03-21 14:30:41 +0100

    📦 NEW: render surveys as cards

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,14 +6,40 @@ class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys()
   }
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card pink darken-3 white-text" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p> {survey.body} </p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <div>Yes: {survey.yes}</div>
+            <div>No: {survey.no}</div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
-    return <div>SurveyList</div>
+    return (
+      <div>
+        {this.props.surveys && this.props.surveys.length ? (
+          <div>{this.renderSurveys()}</div>
+        ) : (
+          <div>SurveyList</div>
+        )}
+      </div>
+    )
   }
 }
 
-function mapStateToProps({ surveys }) {
-  return { surveys }
-}
+const mapStateToProps = ({ surveys }) => ({ surveys })
 
 export default connect(
   mapStateToProps,
