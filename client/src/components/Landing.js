7576922af608eced5e849f80f714f5ed751302fe
Sophia Brandt <tech@s-br.net>
2019-03-22 10:44:26 +0100

    🐛 FIX: dev functions (console.logs, etc.)

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Landing = ({ auth }) => {
-  console.log(auth)
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Feedbacky!</h1>
